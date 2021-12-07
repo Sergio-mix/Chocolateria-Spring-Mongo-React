b7446d765f6bd9df2a1aed32704073e8bd675bd5
Sergio <alejo.80.123@gmail.com>
2021-12-07 16:22:46 -0500

    100%

--- src/main/resources/static/assets/js/manage.js
+++ src/main/resources/static/assets/js/manage.js
@@ -1,4 +1,5 @@
-const API = 'http://localhost:8080/';
+// const API = 'http://localhost:8080/';
+const API = 'http://132.226.37.129:8081/';
 
 const authenticate = API + 'api/user/';
 const userByid = API + 'api/user/get/';
