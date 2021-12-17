68ff14f07c77e4ac53a64012863b295424222f68
Sergio <alejo.80.123@gmail.com>
2021-12-17 12:27:48 -0500

    100%

--- src/main/resources/static/view/src/services/http-config.js
+++ src/main/resources/static/view/src/services/http-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = 'http://localhost:8080/api';
-//const API = 'http://129.158.59.187:8000/';
+//const API = 'http://localhost:8080/api';
+const API = 'http://132.226.37.129:8080/';
 
 export default axios.create({
     baseURL: API,
