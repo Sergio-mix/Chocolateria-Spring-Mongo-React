f43596cc481b8e3d98ada284b2f2f0f4d2ed7554
Sergio <alejo.80.123@gmail.com>
2021-12-17 11:41:23 -0500

    100%

--- src/main/java/co/edu/usa/CRUD/SPRING/MONGO/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.usa.CRUD.SPRING.MONGO.repository;
-
-import co.edu.usa.CRUD.SPRING.MONGO.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository  extends MongoRepository<Product,String> {
-}
